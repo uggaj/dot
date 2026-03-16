307758fcb729f1ea9973e2c4b522da5270c2fa30
jaggu <jagannathvp@proton.me>
2026-03-16 19:28:34 +0530

    fix: remove service.emacs block from emacs.nix

--- user/editor/emacs/emacs.nix
+++ user/editor/emacs/emacs.nix
@@ -38,11 +38,6 @@
     ];
   };
 
-  services.emacs = {
-    enable = true;
-    package = pkgs.emacs;
-  };
-
   fonts.fontconfig.enable = true;
 
   home.file.".config/emacs/init.el".source = ./init.el;
