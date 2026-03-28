b525c6ae54ac6a90156d97772102755a8de36fed
jaggu <jagannathvp@proton.me>
2026-03-28 13:13:33 +0530

    chore: remove ovmf submodule declaration as its available by default

--- configuration.nix
+++ configuration.nix
@@ -118,10 +118,6 @@
       package = pkgs.qemu_kvm;
       runAsRoot = true;
       swtpm.enable = true;
-      ovmf = {
-        enable = true;
-        packages = [ pkgs.OVMFFull.fd ];
-      };
     };
   };
   virtualisation.spiceUSBRedirection.enable = true;
