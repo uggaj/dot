a7f72e9cd55e03d1f38cf36efccee36ab42c7928
jaggu <jagannathvp@proton.me>
2026-04-14 01:11:30 +0530

    chore: untrack archive dir

--- archive/user/terminal/fastfetch.nix
+++
@@ -1,7 +0,0 @@
-{ config, pkgs, ... }:
-
-{
-  home.packages = [ pkgs.fastfetch ];
-  xdg.configFile."fastfetch".source =
-    config.lib.file.mkOutOfStoreSymlink "/home/jaggu/.nixos/user/config/fastfetch";
-}
